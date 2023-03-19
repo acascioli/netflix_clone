812bc0f32d76a423b70f7e1af99f1d614135a902
acascioli <alexcascioli@hotmail.it>
2023-03-19 11:15:28 +0100

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,29 @@
+import React from "react";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import Navbar from "@/components/Navbar";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>Netflix Clone</h1>
     </>
   );
